083200633e0417e95739c2537dd919a10fca4b77
Mikhail Murchikov <mur4ikoff@gmail.com>
2019-12-12 13:27:12 +0300

    Добавление комментариев и мелкие исправления

--- Words Sorting/main.c
+++ Words Sorting/main.c
@@ -7,7 +7,6 @@
 #define MAXWORDS 1000
 #define MAXLEN 256
 
-int readlen();
 int readwords(const char *fileName, char *wordptr[], int len, int maxwords);
 char *wordprocess(char *word);
 int comp(const char *word1, const char *word2);
@@ -18,6 +17,7 @@ void bubblesort(char **wordptr,
 void writelines(char *wordptr[], int nlines);
 void wordfree(char **wordptr, int nwords);
 
+// Массив для хранения запомненных слов
 char *wordptr[MAXWORDS];
 
 
@@ -27,41 +27,33 @@ int main()
     int nwords;
     int lenword;
 
-    printf("Р’РІРµРґРёС‚Рµ РґР»РёРЅСѓ СЃР»РѕРІ: ");
-    lenword = readlen();
-    if (lenword > 0)
+    printf("Введите длину слов: ");
+    // Считывание длины слов - n
+    scanf("%i", &lenword);
+
+    if (lenword > 0 && lenword <= MAXLEN)
+        // Считывание из файла слов длинной n
         if ((nwords = readwords("file.txt", wordptr, lenword, MAXWORDS)) > 0)
         {
-            printf("Р’ С„Р°Р№Р»Рµ РЅР°Р№РґРµРЅРѕ %d РїРѕРґС…РѕРґСЏС‰РёС… СЃР»РѕРІ.\n", nwords);
+            printf("Найдено подходящих слов в файле: %d.\n", nwords);
+            // Сортировка считанных слов
             bubblesort(wordptr, nwords, comp);
-            printf("\nРћС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅРЅС‹Рµ СЃР»РѕРІР°:\n");
+            printf("\nОтсортированные слова:\n");
+            // Печать отсортированных слов
             writelines(wordptr, nwords);
             wordfree(wordptr, nwords);
         }
         else if (nwords == 0)
-            printf("РћС€РёР±РєР°: СЃРѕРѕС‚РІРµС‚СЃС‚РІСѓСЋС‰РёС… СЃР»РѕРІ РЅРµ РЅР°Р№РґРµРЅРѕ.\n");
+            printf("Cоответствующих слов не найдено.\n");
         else if (nwords == -1)
-            printf("РћС€РёР±РєР°: С„Р°Р№Р» СЃ РЅР°Р·РІР°РЅРёРµРј \"file.txt\" РЅРµ РЅР°Р№РґРµРЅ.\n");
+            printf("Ошибка: файл с названием \"file.txt\" не найден.\n");
         else if (nwords == -2)
-            printf("РћС€РёР±РєР°: РІ С„Р°Р№Р»Рµ СЃР»РёС€РєРѕРј РјРЅРѕРіРѕ СЃР»РѕРІ РґР»СЏ СЃРѕСЂС‚РёСЂРѕРІРєРё.\n");
+            printf("Ошибка: в файле слишком много слов для сортировки.\n");
         else
-            printf("РџСЂРѕРёР·РѕС€Р»Р° РЅРµРёР·РІРµСЃС‚РЅР°СЏ РѕС€РёР±РєР°.\n");
+            printf("Произошла неизвестная ошибка.\n");
     else
-        printf("РћС€РёР±РєР°: РЅРµРєРѕСЂСЂРµРєС‚РЅРѕРµ Р·РЅР°С‡РµРЅРёРµ РґР»РёРЅС‹.\n");
+        printf("Ошибка: некорректное значение длины.\n");
     return 0;
-}
-
-
-int readlen()
-{
-    int n;
-
-    scanf("%i", &n);
-    if (!(n > 0))
-        return -1;
-    else if (n > MAXLEN)
-        return -2;
-    return n;
 }
 
 int readwords(const char *fileName, char *wordptr[], int len, int maxwords)
@@ -76,6 +68,7 @@ int readwords(const char *fileName, char *wordptr[], int len, int maxwords)
     nwords = 0;
     while (fgets(line, MAXLEN, ptrFile) != NULL)
     {
+        // Выделяем лексему из строки
         token = strtok(line, " ");
         while (token != NULL)
         {
@@ -83,6 +76,7 @@ int readwords(const char *fileName, char *wordptr[], int len, int maxwords)
                 return -2;
             else
             {
+                // Избавляем лексему от знаков препинания
                 word = wordprocess(token);
                 if (strlen(word) != len)
                 {
@@ -92,6 +86,7 @@ int readwords(const char *fileName, char *wordptr[], int len, int maxwords)
                 if (word != NULL)
                     wordptr[nwords++] = word;
             }
+            // Выделяем следующую лексему из строки
             token = strtok(NULL, " ");
         }
     }
@@ -103,7 +98,6 @@ char *wordprocess(char *token)
 {
     int i, j;
     int len;
-    int inword;
     char *word;
 
     len = strlen(token);
@@ -112,15 +106,11 @@ char *wordprocess(char *token)
         return NULL;
     strcpy(word, token);
 
-    inword = 0;
     for (i = 0; word[i] != '\0'; i++)
     {
-        if (inword == 0 && isalpha(word[i]))
-            inword = 1;
-        else if (inword == 1 && !isalpha(word[i]) && (word[i] != '-'))
-            inword = 0;
-        if (!inword && (ispunct(word[i]) || iscntrl(word[i])))
+        if (ispunct(word[i]) || iscntrl(word[i]))
         {
+            // Удаление i-того символа из строки (из слова)
             for (j = i; j < len - 1; j++)
                 word[j] = word[j + 1];
             len--;
@@ -131,12 +121,13 @@ char *wordprocess(char *token)
     return word;
 }
 
-void writelines(char *wordptr[], int nlines)
+void swap(char *v[], int i, int j)
 {
-    int i;
+    char *tmp;
 
-    for (i = 0; i < nlines; ++i)
-        printf(" %s\n", wordptr[i]);
+    tmp = v[i];
+    v[i] = v[j];
+    v[j] = tmp;
 }
 
 int comp(const char *word1, const char *word2)
@@ -144,7 +135,7 @@ int comp(const char *word1, const char *word2)
     int ch1, ch2;
     int i;
 
-    for (i = 0; word1[i] != '\0' || word2[i] != '\0'; i++)
+    for (i = 0; word1[i] != '\0' && word2[i] != '\0'; i++)
     {
         if (isupper(word1[i]))
             ch1 = word1[i] - 'A';
@@ -168,23 +159,6 @@ int comp(const char *word1, const char *word2)
     return 0;
 }
 
-void swap(char *v[], int i, int j)
-{
-    char *tmp;
-
-    tmp = v[i];
-    v[i] = v[j];
-    v[j] = tmp;
-}
-
-void wordfree(char **wordptr, int nwords)
-{
-    int i;
-
-    for (i = 0; i < nwords; i++)
-        free(wordptr[i]);
-}
-
 void bubblesort(char **wordptr,
                 int nwords,
                 int (*comp)(const char *, const char *))
@@ -204,4 +178,20 @@ void bubblesort(char **wordptr,
         if (flag)
             break;
     }
+}
+
+void writelines(char *wordptr[], int nlines)
+{
+    int i;
+
+    for (i = 0; i < nlines; ++i)
+        printf(" %s\n", wordptr[i]);
+}
+
+void wordfree(char **wordptr, int nwords)
+{
+    int i;
+
+    for (i = 0; i < nwords; i++)
+        free(wordptr[i]);
 }
