08a0d66624242678b9bcf721f40ec40b7abdaaab
Mikhail Murchikov <mur4ikoff@gmail.com>
2019-12-25 23:25:58 +0300

    Убрано добавление повторяющихся слов

--- Words Sorting/main.c
+++ Words Sorting/main.c
@@ -4,7 +4,7 @@
 #include <ctype.h>
 #include <locale.h>
 
-char **readwords(const char *fileName, int *nwords, int len);
+char **readwords(const char *fileName, int *nwords, int *numDiff, int len);
 char *wordprocess(char *word);
 void bubblesort(char **wordptr,
                 int nwords,
@@ -17,7 +17,8 @@ int main()
 {
     setlocale(LC_ALL, "RUS");
     char **wordptr = NULL;
-    int nwords;
+    int nwords;  // Кол-во всего найденных, подходящих слов
+    int numDiff; // Кол-во различных слов.
     int lenword;
 
     printf("Введите длину слов: ");
@@ -25,13 +26,13 @@ int main()
 
     if (lenword > 0)
     {
-        if ((wordptr = readwords("file.txt", &nwords, lenword)) != NULL)
+        if ((wordptr = readwords("file.txt", &nwords, &numDiff, lenword)) != NULL)
         {
-            bubblesort(wordptr, nwords, comp);
+            bubblesort(wordptr, numDiff, comp);
             printf("Найдено подходящих слов в файле: %d.\n", nwords);
             printf("\nОтсортированные слова:\n");
-            writelines(wordptr, nwords);
-            wordfree(wordptr, nwords);
+            writelines(wordptr, numDiff);
+            wordfree(wordptr, numDiff);
         }
         else if (nwords == 0)
             printf("Cоответствующих слов не найдено.\n");
@@ -49,11 +50,12 @@ int main()
     return 0;
 }
 
-char **readwords(const char *fileName, int *nwords, int len)
+char **readwords(const char *fileName, int *nwords, int *numDiff, int len)
 {
     FILE *ptrFile;
     char **wordptr;
-    int numw;
+    int i;
+    int flag;
     char *word, *token, line[256];
 
     if ((ptrFile = fopen(fileName, "rt")) == NULL)
@@ -63,25 +65,54 @@ char **readwords(const char *fileName, int *nwords, int len)
     }
 
     wordptr = NULL;
-    numw = 0;
+    *nwords = 0;
+    *numDiff = 0;
+
+    // Чтение строки
     while (fgets(line, 256, ptrFile) != NULL)
     {
+        // Выделение лексемы
         token = strtok(line, " ");
+        // Пока лексема успешно выделяется
         while (token != NULL)
         {
             word = wordprocess(token);
+            // Если слово успешно обработалось
             if (word != NULL)
+            {
+                // Если длина слова подходит
                 if (strlen(word) == len)
                 {
-                    numw++;
-                    wordptr = (char **)realloc(wordptr, sizeof(char *) * numw);
-                    if (wordptr != NULL)
-                        wordptr[numw - 1] = word;
+                    (*nwords)++;
+                    // Поиск совпадающих слов
+                    flag = 0;
+                    for (i = 0; i < *numDiff; ++i)
+                    {
+                        if (comp(word, wordptr[i]) == 0)
+                        {
+                            flag = 1;
+                            break;
+                        }
+                    }
+                    // Если такого слова ещё нет в массиве
+                    if (!flag)
+                    {
+                        (*numDiff)++;
+                        wordptr = (char **)realloc(wordptr,
+                                                   sizeof(char *) * (*numDiff));
+                        if (wordptr != NULL)
+                            wordptr[*numDiff - 1] = word;
+                        else
+                        {
+                            wordfree(wordptr, *numDiff);
+                            *nwords = -2;
+                            return NULL;
+                        }
+                    }
                     else
                     {
-                        wordfree(wordptr, numw);
-                        *nwords = -2;
-                        return NULL;
+                        free(word);
+                        word = NULL;
                     }
                 }
                 else
@@ -89,17 +120,17 @@ char **readwords(const char *fileName, int *nwords, int len)
                     free(word);
                     word = NULL;
                 }
+            }
             token = strtok(NULL, " ");
         }
     }
     if (!feof(ptrFile))
     {
-        wordfree(wordptr, numw);
+        wordfree(wordptr, *numDiff);
         *nwords = -3;
         return NULL;
     }
     fclose(ptrFile);
-    *nwords = numw;
     return wordptr;
 }
 
@@ -200,4 +231,5 @@ void wordfree(char **wordptr, int nwords)
 
     for (i = 0; i < nwords; i++)
         free(wordptr[i]);
+    free(wordptr);
 }
